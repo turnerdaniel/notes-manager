e6e9175f639d391b2616e749bb25186ee8cb26ff
turnerdaniel <35703802+turnerdaniel@users.noreply.github.com>
2021-01-09 16:51:58 +0000

    Add Javadoc to NoteNotFoundException

--- src/main/java/uk/co/danielturner/notesmanager/errors/NoteNotFoundException.java
+++ src/main/java/uk/co/danielturner/notesmanager/errors/NoteNotFoundException.java
@@ -3,12 +3,21 @@ package uk.co.danielturner.notesmanager.errors;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-// TODO: Does this need exception or runtimeException? Exception requires "throws" keyword.
+/**
+ * Thrown to indicate that the Note being requested does not exist.
+ * @since 1.0.0
+ */
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class NoteNotFoundException extends RuntimeException {
 
-  // https://stackoverflow.com/questions/62044747/message-field-is-empty-in-error-response-spring-boot
+  /**
+   * Constructs a {@code NoteNotFoundException} with a detail message specifying
+   * the exceptional id.
+   * @param id The id which caused the exception
+   * @apiNote Does not display error message to consumers due to <a href=https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-2.3-Release-Notes#changes-to-the-default-error-pages-content>
+   *   Changes to the Default Error Page's Content</a> in Spring Boot 2.3
+   */
   public NoteNotFoundException(Long id) {
-    super("Could not find note with id of " + id.toString());
+    super(String.format("Note with ID %d not found", id));
   }
 }
