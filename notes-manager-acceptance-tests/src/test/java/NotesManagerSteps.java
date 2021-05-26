3f14a7a81a3f6ee8daa38bd0a117f56b02435771
turnerdaniel <35703802+turnerdaniel@users.noreply.github.com>
2021-05-27 00:05:27 +0100

    Add acceptance tests

--- notes-manager-acceptance-tests/src/test/java/NotesManagerSteps.java
+++ notes-manager-acceptance-tests/src/test/java/NotesManagerSteps.java
@@ -2,39 +2,100 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import java.io.UnsupportedEncodingException;
+import java.sql.SQLException;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 public class NotesManagerSteps extends Steps {
 
   MockHttpServletResponse response;
 
-  @Given("^The client has successfully created (\\d*) notes$")
+  String username, password, authToken;
+
+  private final String title = "Title";
+  private final String description = "Description";
+
+  @Before
+  public void clearDatabaseBeforeFeature() throws SQLException {
+    clearDatabase();
+  }
+
+  @Given("^the client has successfully created (\\d*) note(?:s?)$")
   public void setUpNotes(int quantity) throws Exception {
-    setUpMultipleNotes(quantity);
+    if (quantity == 1) {
+      sendCreateNoteRequest();
+    } else {
+      setUpMultipleNotes(this.authToken, quantity);
+    }
   }
 
-  @When("^The client sends a GET request to (\\S*)$")
-  public void sendGetRequest(String url) throws Exception {
-    this.response = get(url);
+  @Given("^an existing user account$")
+  @When("^the client sends a request to create a new account$")
+  public void createUserAccount() throws Exception {
+    this.username = "test.user@example.com";
+    this.password = "PaSsWoRd";
+    final String body = String
+        .format("{\"username\": \"%s\", \"password\": \"%s\"}", this.username, this.password);
+
+    this.response = post("/v2/register", body);
   }
 
-  @When("^The client sends a POST request to (\\S*) with the body:$")
-  public void sendPostRequest(String url, String body) throws Exception {
-    this.response = post(url, body);
+  @When("^the client has authenticated$")
+  public void attemptLogin() throws Exception {
+    final String body = String
+        .format("{\"username\": \"%s\", \"password\": \"%s\"}", this.username, this.password);
+
+    this.response = post("/v2/authenticate", body);
+    this.authToken = createObject(response.getContentAsString()).get("token").asText();
   }
 
-  @When("^The client sends a PUT request to (\\S*) with the body:$")
-  public void sendPutRequest(String url, String body) throws Exception {
-    this.response = put(url, body);
+  @When("^the client sends an (unauthenticated|authenticated) request for account details$")
+  public void sendAccountDetailsRequest(String authType) throws Exception {
+    final String url = "/v2/account";
+    switch (authType) {
+      case "authenticated":
+        this.response = get(url, this.authToken);
+        break;
+      case "unauthenticated":
+      default:
+        this.response = get(url);
+        break;
+    }
   }
 
-  @When("^The client sends a DELETE request to (\\S*)$")
-  public void sendDeleteRequest(String url) throws Exception {
-    this.response = delete(url);
+  @When("^the client sends an authenticated request to create a new note$")
+  public void sendCreateNoteRequest() throws Exception {
+    final String url = "/v2/notes";
+    this.response = post(url, createNoteAsJson(this.title, this.description), this.authToken);
+  }
+
+  @When("^the client sends an authenticated request to retrieve all notes$")
+  public void sendGetAllNoteRequest() throws Exception {
+    final String url = "/v2/notes";
+    this.response = get(url, this.authToken);
+  }
+
+  @When("^the client sends an authenticated request to retrieve the (\\d*)(?:st|nd|rd|th) note$")
+  public void sendGetSingleNoteRequest(int id) throws Exception {
+    final String url = String.format("/v2/notes/%d", id);
+    this.response = get(url, this.authToken);
+  }
+
+  @When("^the client sends an authenticated request to update the (\\d*)(?:st|nd|rd|th) note$")
+  public void sendUpdateNoteRequest(int id) throws Exception {
+    final String url = String.format("/v2/notes/%d", id);
+    final String body = createNoteAsJson(this.title, this.description);
+    this.response = put(url, body, this.authToken);
+  }
+
+  @When("^the client sends an authenticated request to delete the (\\d*)(?:st|nd|rd|th) note$")
+  public void sendDeleteNoteRequest(int id) throws Exception {
+    final String url = String.format("/v2/notes/%d", id);
+    this.response = delete(url, this.authToken);
   }
 
   @Then("^The client should get a (\\d*) response$")
@@ -42,9 +103,21 @@ public class NotesManagerSteps extends Steps {
     assertThat(response.getStatus()).isEqualTo(responseCode);
   }
 
-  @Then("^The response should contain a location header ending with (\\S*)$")
+  @Then("^the response should contain a location header ending with (\\S*)$")
   public void receiveResponseLocation(String location) {
     assertThat(response.getHeader("Location")).endsWith(location);
+  }
+
+  @Then("^the response should contain a note with a matching title$")
+  public void receiveResponseBodyWithMatchingTitle() throws UnsupportedEncodingException, JsonProcessingException {
+    JsonNode responseNote = createObject(response.getContentAsString());
+    assertThat(responseNote.get("title").asText()).isEqualTo(title);
+  }
+
+  @Then("^the response should contain a note with a matching description$")
+  public void receiveResponseBodyWithMatchingDescription() throws UnsupportedEncodingException, JsonProcessingException {
+    JsonNode responseNote = createObject(response.getContentAsString());
+    assertThat(responseNote.get("description").asText()).isEqualTo(description);
   }
 
   @Then("^The response should contain a note with the title of \"(.*)\"$")
@@ -65,7 +138,7 @@ public class NotesManagerSteps extends Steps {
     assertThat(responseNote.get("id").asInt()).isEqualTo(id);
   }
 
-  @Then("^The response should contain (\\d*) notes$")
+  @Then("^the response should contain (\\d*) notes$")
   public void receiveResponseBodyWithQuantity(int quantity)
       throws UnsupportedEncodingException, JsonProcessingException {
     JsonNode responseNotes = createObject(response.getContentAsString());
--- notes-manager-acceptance-tests/src/test/java/Steps.java
+++ notes-manager-acceptance-tests/src/test/java/Steps.java
@@ -1,71 +1,116 @@
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.spring.CucumberContextConfiguration;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import uk.co.danielturner.notesmanager.NotesManagerApplication;
+import uk.co.danielturner.notesmanager.models.Note;
 
 @CucumberContextConfiguration
 @SpringBootTest
 @ContextConfiguration(classes = NotesManagerApplication.class)
-@DirtiesContext(classMode = ClassMode.AFTER_CLASS) //TODO: investigate transactions for clearing DB
 @AutoConfigureMockMvc
 public abstract class Steps {
 
   @Autowired
   MockMvc mockMvc;
 
+  @Autowired
+  DataSource dataSource;
+
+  @Autowired
+  ObjectMapper objectMapper;
+
   protected MockHttpServletResponse get(String url) throws Exception {
-    return mockMvc.perform(
-        MockMvcRequestBuilders.get(url))
-        .andReturn()
-        .getResponse();
+    return sendRequest(MockMvcRequestBuilders.get(url));
+  }
+
+  protected MockHttpServletResponse get(String url, String jwt) throws Exception {
+    return sendRequest(MockMvcRequestBuilders
+        .get(url)
+        .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwt));
   }
 
   protected MockHttpServletResponse post(String url, String body) throws Exception {
-   return mockMvc.perform(
-       MockMvcRequestBuilders.post(url)
-       .contentType(MediaType.APPLICATION_JSON)
-       .content(body))
-       .andReturn()
-       .getResponse();
-  }
-
-  protected MockHttpServletResponse put(String url, String body) throws Exception {
-    return mockMvc.perform(
-        MockMvcRequestBuilders.put(url)
+    return sendRequest(MockMvcRequestBuilders
+        .post(url)
         .contentType(MediaType.APPLICATION_JSON)
-        .content(body))
-        .andReturn()
-        .getResponse();
+        .content(body));
   }
 
-  protected MockHttpServletResponse delete(String url) throws Exception {
-    return mockMvc.perform(
-        MockMvcRequestBuilders.delete(url))
-        .andReturn()
-        .getResponse();
+  protected MockHttpServletResponse post(String url, String body, String jwt) throws Exception {
+    return sendRequest(MockMvcRequestBuilders
+        .post(url)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(body)
+        .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwt));
+  }
+
+  protected MockHttpServletResponse put(String url, String body, String jwt) throws Exception {
+    return sendRequest(MockMvcRequestBuilders
+        .put(url)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(body)
+        .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwt));
+  }
+
+  protected MockHttpServletResponse delete(String url, String jwt) throws Exception {
+    return sendRequest(
+        MockMvcRequestBuilders.delete(url).header(HttpHeaders.AUTHORIZATION, "Bearer " + jwt));
   }
 
   protected JsonNode createObject(String json) throws JsonProcessingException {
-    ObjectMapper mapper = new ObjectMapper();
-    return mapper.readTree(json);
+    return objectMapper.readTree(json);
   }
 
-  protected void setUpMultipleNotes(int quantity) throws Exception {
-    final String url = "/v1/notes";
-    final String noteTemplate = "{\"title\": \"Example Title %d\"}";
+  protected String createNoteAsJson(String title, String description)
+      throws JsonProcessingException {
+    return objectMapper.writeValueAsString(new Note(title, description));
+  }
+
+  protected void setUpMultipleNotes(String token, int quantity) throws Exception {
+    final String url = "/v2/notes";
     for (int i = 1; i < quantity + 1; i++) {
-      post(url, String.format(noteTemplate, i));
+      String note = createNoteAsJson("Bulk Title" + i, "Bulk Description" + i);
+      post(url, note, token);
     }
+  }
+
+  protected void clearDatabase() throws SQLException {
+    Connection conn = dataSource.getConnection();
+
+    conn.createStatement().executeUpdate("SET FOREIGN_KEY_CHECKS=0");
+    conn.createStatement().executeUpdate("ALTER SEQUENCE hibernate_sequence RESTART WITH 1");
+
+    ResultSet rs = conn.createStatement().
+        executeQuery(
+            "SELECT table_name FROM information_schema.tables WHERE table_schema = SCHEMA()");
+    while (rs.next()) {
+      final String table = rs.getString(1);
+      conn.createStatement().executeUpdate("TRUNCATE TABLE \"" + table + "\" RESTART IDENTITY");
+    }
+    rs.close();
+
+    conn.createStatement().executeUpdate("SET FOREIGN_KEY_CHECKS=1");
+    conn.close();
+  }
+
+  private MockHttpServletResponse sendRequest(RequestBuilder requestBuilder) throws Exception {
+    return mockMvc.perform(requestBuilder).andDo(print()).andReturn().getResponse();
   }
 }
